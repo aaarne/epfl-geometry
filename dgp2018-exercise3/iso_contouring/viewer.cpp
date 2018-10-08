461a3e7d92ffac6d254ec047bcfc6cd531c6d444
Arne Sachtler <arne.sachtler@gmail.com>
2018-10-08 21:31:52 +0200

    Implement marching triangles

--- dgp2018-exercise3/iso_contouring/viewer.cpp
+++ dgp2018-exercise3/iso_contouring/viewer.cpp
@@ -28,22 +28,21 @@ typedef Surface_mesh Mesh;
 // ========================================================================
 // EXERCISE 1
 // ========================================================================
-Scalar Viewer::iso_value(Point v_pos)
-{
-    float x,y;
+Scalar Viewer::iso_value(Point v_pos) {
+    float x, y;
     x = v_pos.x;
     y = v_pos.y;
 
     // ----- (un)comment a line to change the function you are testing
     //Scalar iso = sqrt(x*x + y*y) - 1;
-    //Scalar iso = sin(2*x+2*y) - cos(4*x*y) +1;
-    Scalar iso = y*y - sin(x*x);
+//    Scalar iso = sin(2*x+2*y) - cos(4*x*y) +1;
+    Scalar iso = y * y - sin(x * x);
 
     return iso;
 }
 
 void Viewer::calc_iso_contouring() {
-    Mesh::Vertex_property<Scalar> v_iso = mesh.vertex_property<Scalar>("v:iso", 0);
+    Mesh::Vertex_property <Scalar> v_iso = mesh.vertex_property<Scalar>("v:iso", 0);
     segment_points.clear();
     std::vector<Point> v_positions(mesh.n_vertices());
     std::vector<std::vector<int> > triangle_ids;
@@ -58,7 +57,7 @@ void Viewer::calc_iso_contouring() {
         v_iso[v] = iso; //this variable is for coloring the density; do not change this variable
     }
 
-    for(auto f: mesh.faces()) {
+    for (auto f: mesh.faces()) {
         std::vector<int> vv(3);
         int k = 0;
         for (auto v: mesh.vertices(f)) {
@@ -68,13 +67,57 @@ void Viewer::calc_iso_contouring() {
         triangle_ids.push_back(vv);
     }
 
+
     //segment_points is defined in viewer.h as std::vector<Point> segment_points;
     //add points in segment_points forming an edge one after the other;
     //for example segment_points[0] and segment_points[1] are two points forming the first edge
     //and segment_points[2] and segment_points[3] are two points forming the second edge
 
     // ----- add your code here -----
+    vector<pair<int, int>> raw_points;
+    for (const auto &triangle : triangle_ids) {
+        vector<Scalar> isos;
+        std::transform(triangle.begin(), triangle.end(), std::back_inserter(isos),
+                       [this, &v_positions](int vertex_id) -> Scalar {
+                           return this->iso_value(v_positions[vertex_id]);
+                       });
 
+        int cases = 0b1 * (isos[0] >= 0)
+                    + 0b10 * (isos[1] >= 0)
+                    + 0b100 * (isos[2] >= 0);
+
+        switch (cases) {
+            case 0:
+            case 0b111:
+                continue;
+            case 0b1:
+            case 0b110:
+                raw_points.emplace_back(triangle[0], triangle[1]);
+                raw_points.emplace_back(triangle[0], triangle[2]);
+                break;
+            case 0b10:
+            case 0b101:
+                raw_points.emplace_back(triangle[0], triangle[1]);
+                raw_points.emplace_back(triangle[1], triangle[2]);
+                break;
+            case 0b100:
+            case 0b11:
+                raw_points.emplace_back(triangle[1], triangle[2]);
+                raw_points.emplace_back(triangle[0], triangle[2]);
+                break;
+            default:
+                throw std::exception();
+        }
+
+    }
+
+    for (const auto &point : raw_points) {
+        Point p1 = v_positions[point.first];
+        Point p2 = v_positions[point.second];
+        Scalar iso1 = abs(this->iso_value(p1));
+        Scalar iso2 = abs(this->iso_value(p2));
+        segment_points.push_back(p1 + (p2 - p1)*iso1/(iso1+iso2));
+    }
 
     // ------------------------------
 }
